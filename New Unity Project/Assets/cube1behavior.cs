f7c180c1820239c67e2ed71b452c5e49bf495e4a
Mitrajit Chandra Chandra <chandra.rupam@gmail.com>
2020-07-25 16:27:54 +0530

    First start

---
+++ New Unity Project/Assets/cube1behavior.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading;
+using UnityEngine;
+
+public class cube1behavior : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, 2000 * Time.deltaTime);
+    }
+}
